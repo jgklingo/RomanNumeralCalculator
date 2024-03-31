72715ca662f659d1419599d03fd7fe98c2fd437a
jgklingo <jgklingo@byu.edu>
2024-03-30 20:15:27 -0600

    decimal_to_roman and roman_to_decimal implemented. Added support for command line arguments.

--- main.py
+++ main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+conversion_dictionary = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def decimal_to_roman(decimal: int) -> str:
+    roman_numerals = ""
+    while decimal > 0:
+        for symbol in conversion_dictionary.keys().__reversed__():
+            if conversion_dictionary[symbol] <= decimal:
+                roman_numerals += symbol
+                decimal -= conversion_dictionary[symbol]
+                break
+        last_3 = roman_numerals[-3:]
+        last_4 = roman_numerals[-4:]
+        last_5 = roman_numerals[-5:]
+        if last_4 == "IIII":
+            roman_numerals = roman_numerals[:-4]
+            roman_numerals += "IV"
+        elif last_5 == "VIIII":
+            roman_numerals = roman_numerals[:-5]
+            roman_numerals += "IX"
+        elif last_4 == "XXXX":
+            roman_numerals = roman_numerals[:-4]
+            roman_numerals += "XL"
+        elif last_5 == "LXXXX":
+            roman_numerals = roman_numerals[:-5]
+            roman_numerals += "XC"
+        elif last_4 == "CCCC":
+            roman_numerals = roman_numerals[:-4]
+            roman_numerals += "CD"
+        elif last_5 == "DCCCC":
+            roman_numerals = roman_numerals[:-5]
+            roman_numerals += "CM"
+        last_3 = roman_numerals[-3:]
+        if last_3 == "VIV":
+            roman_numerals = roman_numerals[:-3]
+            roman_numerals += "IX"
+        elif last_3 == "LXL":
+            roman_numerals = roman_numerals[:-3]
+            roman_numerals += "XC"
+        elif last_3 == "DCD":
+            roman_numerals = roman_numerals[:-3]
+            roman_numerals += "CM"
+    return roman_numerals
 
 
-# Press the green button in the gutter to run the script.
+def roman_to_decimal(roman_numeral: str) -> int:
+    symbol_2_value = 0
+    total = 0
+    while len(roman_numeral) > 0:
+        symbol_1_value = conversion_dictionary[roman_numeral[0]]
+        if len(roman_numeral) > 1:
+            symbol_2_value = conversion_dictionary[roman_numeral[1]]
+        if symbol_1_value < symbol_2_value:
+            total += symbol_2_value - symbol_1_value
+            roman_numeral = roman_numeral[2:]
+        else:
+            total += symbol_1_value
+            roman_numeral = roman_numeral[1:]
+    return total
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    help_string = "Syntax: [-td(--todecimal) | -tr(--toroman)] <number_to_convert>"
+    if len(sys.argv) < 3:
+        print(help_string)
+    elif sys.argv[1] == "-td" or sys.argv[1] == "--todecimal":
+        print(roman_to_decimal(sys.argv[2]))
+    elif sys.argv[1] == "-tr" or sys.argv[1] == "--toroman":
+        print(decimal_to_roman(int(sys.argv[2])))
+    else:
+        print("Option not recognized." + help_string)
